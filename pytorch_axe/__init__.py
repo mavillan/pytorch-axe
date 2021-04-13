76c0e558a489aa46a292d7fb7f922062e4bcebf3
Martín Villanueva <nallivam@gmail.com>
2021-04-12 23:29:00 -0500

    Adds import shortcuts in __init__.py

--- pytorch_axe/__init__.py
+++ pytorch_axe/__init__.py
@@ -1,4 +1,4 @@
-import monitor
-import training
-from .monitor import Monitor
-from .training import iterative_train,iterative_predict
+import pytorch_axe.monitor
+import pytorch_axe.training
+from pytorch_axe.monitor import Monitor
+from pytorch_axe.training import iterative_train,iterative_predict
